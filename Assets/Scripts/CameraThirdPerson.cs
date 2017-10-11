5877f6e255b39ea115b0d3f4b1cc281b75340244
Touhma <touhma@outlook.com>
2017-10-11 19:49:17 +0200

    Ajouts des scripts cam + mouvement

---
+++ Assets/Scripts/CameraThirdPerson.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraThirdPerson : MonoBehaviour {
+
+    public GameObject target;
+    public float damping = 1;
+    public Vector3 offset, localRotation;
+    public float rotationSpeed = 10f;
+    float scrollSensitvity = 4f;
+    void Start() {
+        offset = transform.position - target.transform.position;
+        transform.LookAt(target.transform.position);
+    }
+
+    void update() {
+
+    }
+    
+    void LateUpdate() {
+        if (Input.GetAxis("Horizontal") != 0) {
+            localRotation.y += Input.GetAxis("Horizontal") * this.rotationSpeed;
+        } else {
+            localRotation.y = 0;
+        }
+        float step = 0f;
+        Vector3 offsetToAdd = new Vector3();
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f) {
+            step = Input.GetAxis("Mouse ScrollWheel") * scrollSensitvity;
+            offsetToAdd =  (offset / offset.magnitude) * (offset.magnitude * 0.1f * step);
+        }
+
+        offset = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * rotationSpeed, Vector3.up) * (offset - offsetToAdd);
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * damping);
+        transform.position = target.transform.position + offset;
+        transform.LookAt(target.transform.position);
+    }
+}
---
+++ Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour {
+
+    NavMeshAgent playerAgent;
+
+    // Use this for initialization
+    void Start() {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Verif Si on click & si on ne click pas sur l'interface
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+            this.GetInteraction();
+        }
+
+    }
+
+    void GetInteraction() {
+        // recup d'un raycast partant de la caméra à la postion de la souris
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        // si çà touche on output les infos sur interactionInfo 
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity)) {
+            //recup du GO touché
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Object") {
+                //si c'est un objet qu'on peut interagir ( avec un tag ) 
+                Debug.Log("Interactable Interracted");
+            }else{
+                // sinon on veut bouger + pathfinding
+                playerAgent.destination = interactionInfo.point;
+            }   
+        }
+    }
+}
