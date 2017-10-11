b27ccd073a9cc1683f8d1b316af5d26f27bdf15a
Touhma <touhma@outlook.com>
2017-10-11 21:51:48 +0200

    transition between scenes

--- Assets/Scripts/CombatTrigger.cs
+++ Assets/Scripts/CombatTrigger.cs
@@ -1,40 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CombatTrigger : MonoBehaviour {
 
     GameObject player;
+    private bool isTriggered;
 
-
-    void Awake ()
-    {
-        player = GameObject.FindGameObjectWithTag ("Player");
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
 
-    void OnTriggerEnter (Collider other)
-    {
-        if(other.gameObject == player)
-        {
-			TriggerCombat();
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject == player) {
+            TriggerCombat();
         }
     }
 
 
-    void OnTriggerExit (Collider other)
-    {
+    void OnTriggerExit(Collider other) {
     }
 
 
-    void Update ()
-    {
+    void Update() {
     }
 
 
-	void TriggerCombat ()
-	{
-		// Combat !!
-		Debug.Log("combat!");
-	}
+    void TriggerCombat() {
+        // Combat !!
+        if (!isTriggered) {
+            isTriggered = true;
+            Debug.Log("combat!");
+            if (SceneManager.GetActiveScene().name == "LevelTest2") {
+                SceneManager.LoadScene("LevelTest", LoadSceneMode.Single);
+            } else {
+                SceneManager.LoadScene("LevelTest2", LoadSceneMode.Single);
+            }
+
+        }
+
+
+    }
 }
